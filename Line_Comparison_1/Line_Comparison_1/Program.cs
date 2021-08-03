2a9127877bb2e19a655e55b7a37b0eeec0cb63c1
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-03 16:24:15 +0530

    Line Comparison 1

---
+++ Line_Comparison_1/Line_Comparison_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Line_Comparison_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 4;
+            int y1 = 5;
+            int x2 = 7;
+            int y2 = 9;
+            double LENGTH_OF_LINE;
+            LENGTH_OF_LINE = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("The Length of Line = " + LENGTH_OF_LINE);
+        }
+    }
+}
