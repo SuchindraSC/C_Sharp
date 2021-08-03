fbdb14ca4fd0def724e4b9ff9ec0135d6c47c419
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-03 11:44:10 +0530

    feat: Employee Attendance

---
+++ Employee_Attendance/Employee_Attendance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Employee_Attendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random rand = new Random();
+            int present = rand.Next(0, 2);
+            if(present == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
