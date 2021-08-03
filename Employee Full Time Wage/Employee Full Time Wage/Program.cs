1d1f75097e2830782a588f2d10f8f5f3a8a7c847
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-03 12:33:14 +0530

    Employee Wages

---
+++ Employee Full Time Wage/Employee Full Time Wage/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Employee_Full_Time_Wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FULL_TIME_HOURS = 8;
+            int IS_FULL_TIME = 1;
+            int PER_HOUR_WAGE = 20;
+            int DAILY_WAGE = 0;
+            Random rand = new Random();
+            int present = rand.Next(0,2);
+            if (present == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+                DAILY_WAGE = FULL_TIME_HOURS * PER_HOUR_WAGE;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent!");
+            }
+            Console.WriteLine("Daily Wage of Employee is "+DAILY_WAGE);
+        }
+    }
+}
